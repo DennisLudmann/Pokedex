1a163daf8398289b30f35ea2bb9997df5e866686
DennisLudmann <dennisludmann@gmail.com>
2022-02-03 20:55:53 +0100

    on click new Pokedex (not done design + content)

--- scripts.js
+++ scripts.js
@@ -12,23 +12,25 @@ async function displayOverview() {
         let response = await fetch(url);                    //waiting so the function doesnt continue without this input
         currentPokemon = await response.json();             // to json so we have a file type we can work with
 
-        let pokemonEntry = '';                              // predefined and empty so it can be filled later (3 parts)
+        let pokemonEntryBuild = '';                              // predefined and empty so it can be filled later (3 parts)
 
         let Types = currentPokemon["types"].length;         // checking if one or two types are defined
-        pokemonEntry += `
+        pokemonEntryBuild += `
             <div onclick="loadPokemon('${currentPokemon['name']}')" class="card-wrapper bg-${currentPokemon["types"]["0"]["type"]["name"]}">
                 <img src="${currentPokemon["sprites"]["other"]["dream_world"]["front_default"]}" class="card-image " alt="${currentPokemon['name']}">
                 <div class="card-description">
                     <h2 class="card-title">${currentPokemon['name'].substring(0,1).toUpperCase() + currentPokemon['name'].substring(1)}</h2>
                     <p class="card-text"> ${currentPokemon["types"]["0"]["type"]["name"]}</p>`
                     if (Types > 1) {                                    // to display only if both types are set
-                        pokemonEntry += `<p class="card-text">${currentPokemon["types"]["1"]["type"]["name"]}</p>`
+                        pokemonEntryBuild += `<p class="card-text">${currentPokemon["types"]["1"]["type"]["name"]}</p>`
                     }
-                    pokemonEntry += `
+                    pokemonEntryBuild += `
                 </div>
             </div>`
-            document.getElementById('overview').innerHTML += pokemonEntry; // let the different elements be in place befor you display it
+            document.getElementById('overview').innerHTML += pokemonEntryBuild; // let the different elements be in place befor you display it
+            
         }
+        
     }
 
 /*
@@ -52,6 +54,7 @@ function renderPokemonInfo() {                                                  
     document.getElementById('skill-speed').innerHTML = currentPokemon["stats"][5]["base_stat"] + ` speed`;
     document.getElementById('type-1').innerHTML = currentPokemon["types"]["0"]["type"]["name"];
     document.getElementById('type-2').innerHTML = currentPokemon["types"]["1"]["type"]["name"];
+   
 }
 
 
