0a2e3736d3a655a810875cab55b85f00411ffb52
Dennis Ludmann <dennisludmann@gmail.com>
2022-12-02 11:55:00 +0100

    card css redesigned, onscroll loading added - functions involved need refactoring

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ async function displayOverview() {                                  // main card
         isLoading = true;
         let content = document.getElementById('overview');
         content.innerHTML = '';
-        loadContent();
+        await loadContent();
         isLoading = false;
     }
     else {                                                          // retry if isLoading was still true 
@@ -36,19 +36,7 @@ for (let i = 1; i < loadCounter; i++) {                             // load poke
     renderPokemonOverview();                                        // builds the different pokecards
 }
 }
-
-
-async function fillPokemonNames() {
-    let url = `https://pokeapi.co/api/v2/pokemon/?limit=151`;               // 151 is the number of first gen pokemon
-    let response = await fetch(url);                                        //waiting so the function doesnt continue without this input
-    let responseJsonNames = await response.json();                          // to json so we have a file type we can work with
-    for (let i = 0; i < responseJsonNames['results'].length; i++) {
-        let name = responseJsonNames['results'][i];
-        listOfPokemonNames.push(name);
-    }
-};
-
-
+ 
 function renderPokemonOverview() {
     let pokemonEntryBuild = '';                                 // predefined and empty so it can be filled later
     let Types = currentPokemon["types"].length;                 // checking if one or two types are defined
@@ -62,23 +50,30 @@ function renderPokemonOverview() {
 }
 
 
+async function fillPokemonNames() {
+    let url = `https://pokeapi.co/api/v2/pokemon/?limit=151`;               // 151 is the number of first gen pokemon
+    let response = await fetch(url);                                        //waiting so the function doesnt continue without this input
+    let responseJsonNames = await response.json();                          // to json so we have a file type we can work with
+    for (let i = 0; i < responseJsonNames['results'].length; i++) {
+        let name = responseJsonNames['results'][i];
+        listOfPokemonNames.push(name);
+    }
+}
+
+
 async function searchPokemon() {
     let search = document.getElementById('search').value;
     let content = document.getElementById('overview');
     content.innerHTML = '';
     search = search.toLowerCase();
-    debugger;
     if (search == "") {                                              // go back to the initial listing if no search is active
         displayOverview();
         return
     }
     if (!isLoading) {
         isLoading = true;
-        loadContentSearch(search); 
-        setTimeout(() => {
-            isLoading = false;
-        }, 100);                                 // sends the searched phrase to loader
-       
+        await loadContentSearch(search); 
+        isLoading = false;                                       // sends the searched phrase to loader
     }
     else {                                                            // retry if isLoading was still true
         setTimeout(() => {
@@ -99,21 +94,6 @@ async function loadContentSearch(search) {                                // loa
             renderPokemonOverview();
         }
     }
-}
-
-
-function renderPokemonInfo() {                                                                         // shows a variety of info on selected pokemon 
-    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'].toUpperCase();
-    document.getElementById('img-pokedex').src = currentPokemon["sprites"]["front_default"];            //its the .img so src =
-    document.getElementById('skill-hp').innerHTML = currentPokemon["stats"][0]["base_stat"];
-    document.getElementById('skill-attack').innerHTML = currentPokemon["stats"][1]["base_stat"];
-    document.getElementById('skill-defence').innerHTML = currentPokemon["stats"][2]["base_stat"];
-    document.getElementById('skill-speed').innerHTML = currentPokemon["stats"][5]["base_stat"];
-    document.getElementById('type-1').innerHTML = currentPokemon["types"]["0"]["type"]["name"];
-    document.getElementById('id').innerHTML = '#' + currentPokemon["id"];
-    document.getElementById('height').innerHTML = 'Height:' + '&nbsp &nbsp &nbsp &nbsp &nbsp' + currentPokemon["height"] * 10 + `cm`;
-    document.getElementById('weight').innerHTML = 'Weight:' + '&nbsp &nbsp &nbsp &nbsp &nbsp' + currentPokemon["weight"] / 10 + `kg`;
-    document.getElementById('pokemon-name-container').classList.add('bg-${currentPokemon["types"]["0"]["type"]["name"]');
 }
 
 
@@ -140,27 +120,53 @@ function hideLoader() {
     document.getElementById('loader-container').remove();
 }
 
-
-function addMorePokemon() {                                // when add pokemon is triggered, adds 15 more and renders
+// when add pokemon is triggered, adds 15 more and renders
+function addMorePokemon() {                                
     loadCounter = loadCounter + 15;
     displayOverview();
 }
 
 
-function cardGeneratorOne(pokemonEntryBuild) {          // returns html to function (1 skill)
+
+// when we are in the bottom of the page then load 15 pokemon more
+window.onscroll = function () {
+    if (window.scrollY + window.innerHeight >= document.body.clientHeight) {
+        addMorePokemon();
+    }
+  };
+
+   // shows a variety of info on selected pokemon 
+function renderPokemonInfo() {                                                                      
+    document.getElementById('pokemon-name').innerHTML = currentPokemon['name'].toUpperCase();
+    document.getElementById('img-pokedex').src = currentPokemon["sprites"]["front_default"];            //its the .img so src =
+    document.getElementById('skill-hp').innerHTML = currentPokemon["stats"][0]["base_stat"];
+    document.getElementById('skill-attack').innerHTML = currentPokemon["stats"][1]["base_stat"];
+    document.getElementById('skill-defence').innerHTML = currentPokemon["stats"][2]["base_stat"];
+    document.getElementById('skill-speed').innerHTML = currentPokemon["stats"][5]["base_stat"];
+    document.getElementById('type-1').innerHTML = currentPokemon["types"]["0"]["type"]["name"];
+    document.getElementById('id').innerHTML = '#' + currentPokemon["id"];
+    document.getElementById('height').innerHTML = 'Height:' + '&nbsp &nbsp &nbsp &nbsp &nbsp' + currentPokemon["height"] * 10 + `cm`;
+    document.getElementById('weight').innerHTML = 'Weight:' + '&nbsp &nbsp &nbsp &nbsp &nbsp' + currentPokemon["weight"] / 10 + `kg`;
+    document.getElementById('pokemon-name-container').classList.add('bg-${currentPokemon["types"]["0"]["type"]["name"]');
+}
+
+
+ // returns html to function (1 skill)
+function cardGeneratorOne(pokemonEntryBuild) {         
     return `
     <div id="wrapper" onclick="loadPokemon('${currentPokemon['name']}')" class="card-wrapper bg-${currentPokemon["types"]["0"]["type"]["name"]}">
         <img src="${currentPokemon["sprites"]["other"]["dream_world"]["front_default"]}" class="card-image " alt="${currentPokemon['name']}">
         <div class="card-description">
             <h2 class="card-title">${currentPokemon['name'].substring(0, 1).toUpperCase() + currentPokemon['name'].substring(1)} </h2> 
              <p class="card-text"> ${currentPokemon["types"]["0"]["type"]["name"]}</p>
-            <div class="flex-end"><span class="id-card"># ${currentPokemon["id"]}</span></div>
+            <div class="card-id"><span class="id-card"># ${currentPokemon["id"]}</span></div>
         </div>
     </div>`
 }
 
 
-function cardGeneratorTwo(pokemonEntryBuild) {          // returns html to function (2 skill)
+    // returns html to function (2 skills)
+function cardGeneratorTwo(pokemonEntryBuild) {      
     return `
     <div id="wrapper" onclick="loadPokemon('${currentPokemon['name']}')" class="card-wrapper bg-${currentPokemon["types"]["0"]["type"]["name"]}">
         <img src="${currentPokemon["sprites"]["other"]["dream_world"]["front_default"]}" class="card-image " alt="${currentPokemon['name']}">
@@ -168,7 +174,7 @@ function cardGeneratorTwo(pokemonEntryBuild) {          // returns html to funct
             <h2 class="card-title">${currentPokemon['name'].substring(0, 1).toUpperCase() + currentPokemon['name'].substring(1)} </h2> 
             <p class="card-text"> ${currentPokemon["types"]["0"]["type"]["name"]}</p>                                      
             <p class="card-text">${currentPokemon["types"]["1"]["type"]["name"]}</p>
-            <div class="flex-end"><span class="id-card"># ${currentPokemon["id"]}</span></div>
+            <div class="card-id"><span class="id-card"># ${currentPokemon["id"]}</span></div>
         </div>
     </div>`
 }
